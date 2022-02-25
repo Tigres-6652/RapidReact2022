136225e4c9527740c3c8ee3176512a248762ac51
unknown <you@example.comandres.monsivaiss@uanl.edu.mx>
2022-02-24 23:16:39 -0600

    codigo nuevo de drivetrain e intake en teleop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.Controllers;
 import frc.robot.Constants.Motores;
 import frc.robot.Constants.Neumatica;
@@ -56,6 +57,8 @@ Joystick JoystickDriver1 = new Joystick (Controllers.kJoystickDriver1);
 //Joystick JoystickDriver2 = new Joystick (Controllers.KJoystickDriver2);
 
 double velocidad;
+boolean IntakeState;
+boolean compresorState;
 
 
   @Override
@@ -65,6 +68,11 @@ double velocidad;
 
   @Override
   public void robotPeriodic() {
+
+    //IMPRIME LOS VALORES EN EL SMARTDASHBOARD
+  SmartDashboard.putBoolean("Intake", IntakeState);
+  SmartDashboard.putBoolean("Compresor", compresorState);
+
 
     
 
@@ -87,6 +95,32 @@ double velocidad;
   //Movimiento del chasis con control Xbox
   velocidad=JoystickDriver1.getRawAxis(3)-JoystickDriver1.getRawAxis(2); 
   chasis.arcadeDrive(velocidad, JoystickDriver1.getRawAxis(0));
+
+
+  //ACCIONAMIENTO DE INTAKE CON BOTON "A"
+
+  if(JoystickDriver1.getRawButton(0)){ 
+    if(IntakeState = true) { 
+      PISTINTAKE.set(true);
+     }
+     else{
+      PISTINTAKE.set(false);
+   }
+   IntakeState = !IntakeState; 
+   }
+
+
+   //SE PRENDE EL COMPRESOR CON EL BOTON "B"
+//Mas adelante cambiar esto al driver secundario
+   if(JoystickDriver1.getRawButton(1)){ 
+    if(compresorState = true) { 
+      PISTINTAKE.set(true);
+     }
+     else{
+      PISTINTAKE.set(false);
+   }
+   compresorState = !compresorState; 
+   }
 
 
 

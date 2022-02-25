6fbc6c4cc66e5df1e78c5af02c53591ee4c94ec0
unknown <you@example.comandres.monsivaiss@uanl.edu.mx>
2022-02-24 23:07:30 -0600

    se agrego el codigo de intake, y chasis para teleop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,36 +2,64 @@
 package frc.robot;
 
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import frc.robot.Constants.Controllers;
+import frc.robot.Constants.Motores;
+import frc.robot.Constants.Neumatica;
 
 
 
 
 public class Robot extends TimedRobot {
 
-//DECLARACIONES 
+ 
 
-//Motores
-int KMOTORD1 = 1;//Chassis derecha 1
-int KMOTORD2 = 2;//Chassis derecha 2
-int KMOTORD3 = 3;//Chassis derecha 3
-int KMOTORI1 = 4;//Chassis izquierda 1
-int KMOTORI2 = 5;//Chassis izquierda 2
-int KMOTORI3 = 6;//Chassis izquierda 3
-int KMOTORS1 = 7;//Shooter 1
-int KMOTORS2 = 8;//Shooter 2
-int KMOTORIN = 9;//Intake 
-int KMOTORCP = 10;//Capucha 
-int KMOTORID = 11;//Indexer
+
+
+
+//Chasis
+
+WPI_TalonSRX MOTORD1 = new WPI_TalonSRX(Motores.KMOTORD1);
+WPI_TalonSRX MOTORD2 = new WPI_TalonSRX(Motores.KMOTORD2);
+WPI_TalonSRX MOTORD3 = new WPI_TalonSRX(Motores.KMOTORD3);
+
+WPI_TalonSRX MOTORI4 = new WPI_TalonSRX(Motores.KMOTORI4);
+WPI_TalonSRX MOTORI5 = new WPI_TalonSRX(Motores.KMOTORI5);
+WPI_TalonSRX MOTORI6 = new WPI_TalonSRX(Motores.KMOTORI6);
+
+  MotorControllerGroup motsI = new MotorControllerGroup(MOTORD1, MOTORD2, MOTORD3);
+  MotorControllerGroup motsD = new MotorControllerGroup(MOTORI4, MOTORI5, MOTORI6);
+
+  DifferentialDrive chasis = new DifferentialDrive(motsI, motsD);
+
 
 //Neumática 
-int KPISTIN1 = 1;
-int KPISTIN2 = 2;
+
+
+Compressor COMPRESOR = new Compressor(0, PneumaticsModuleType.CTREPCM);
+
+Solenoid PISTINTAKE = new Solenoid(PneumaticsModuleType.CTREPCM, Neumatica.KPISTINTAKE);
+Solenoid PISTCHASIS = new Solenoid(PneumaticsModuleType.CTREPCM, Neumatica.KPISTCHASIS);
+
+
+//CONTROLES
+
+Joystick JoystickDriver1 = new Joystick (Controllers.kJoystickDriver1);
+//Joystick JoystickDriver2 = new Joystick (Controllers.KJoystickDriver2);
+
+double velocidad;
+
 
   @Override
   public void robotInit() {
-
- 
 
   }
 
@@ -48,18 +76,21 @@ int KPISTIN2 = 2;
   }
 
   @Override
-  public void autonomousPeriodic() {
-
- 
-
-
-  }
+  public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+  //Movimiento del chasis con control Xbox
+  velocidad=JoystickDriver1.getRawAxis(3)-JoystickDriver1.getRawAxis(2); 
+  chasis.arcadeDrive(velocidad, JoystickDriver1.getRawAxis(0));
+
+
+
+  }
 
   @Override
   public void disabledInit() {}
